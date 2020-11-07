6556e686886a246ac5792322bae424a74eb2f587
Alexey Gavrilov <eeealesha1993@yandex.ru>
2020-11-07 16:11:24 +0300

    new

--- src/components/App.js
+++ src/components/App.js
@@ -10,12 +10,14 @@ function App() {
     // Создаем стейт-переменные попапов
     const [isEditProfilePopupOpen, setEditProfilePopupOpen] = React.useState(false); // true или false
     const [isAddPlacePopupOpen, setAddPlacePopupOpen] = React.useState(false); // true или false
-    const [isEditAvatarPopupOpen, setAvatarPopupOpen] = React.useState(false); // true или
+    const [isEditAvatarPopupOpen, setAvatarPopupOpen] = React.useState(false); // true или false
+    const [isImagePopupOpen, setImagePopupOpen] = React.useState(false); // true или false
     // Создаем стейт-переменную выбранной карточки
     const [selectedCard, setSelectedCard] = React.useState(null);
 
     function handleCardClick(card) {
         setSelectedCard(card);
+        setImagePopupOpen(true)
     }
 
     function handleEditAvatar() {
@@ -47,61 +49,62 @@ function App() {
                 <PopupWithForm onClose={closeAllPopups} isOpen={isEditProfilePopupOpen} title='Редактировать профиль'
                                name='profile' buttonText='Сохранить' children={
 
-                        <>
-                    <label className="form__field">
-                        <input
-                            type="text"
-                            className="form__item form__item_el_name"
-                            id="name"
-                            name="name"
-                            required
-                            minLength="2"
-                            maxLength="40"
-                        />
-                        <div className="form__error-text" id="name-error"></div>
-                    </label>
-                    <label className="form__field">
-                        <input
-                            type="text"
-                            className="form__item form__item_el_job"
-                            id="job"
-                            name="job"
-                            required
-                            minLength="2"
-                            maxLength="200"
-                        />
-                        <div className="form__error-text" id="job-error"></div>
-                    </label></>}/>
+                    <>
+                        <label className="form__field">
+                            <input
+                                type="text"
+                                className="form__item form__item_el_name"
+                                id="name"
+                                name="name"
+                                required
+                                minLength="2"
+                                maxLength="40"
+                            />
+                            <div className="form__error-text" id="name-error"></div>
+                        </label>
+                        <label className="form__field">
+                            <input
+                                type="text"
+                                className="form__item form__item_el_job"
+                                id="job"
+                                name="job"
+                                required
+                                minLength="2"
+                                maxLength="200"
+                            />
+                            <div className="form__error-text" id="job-error"></div>
+                        </label>
+                    </>}/>
                 <PopupWithForm onClose={closeAllPopups} isOpen={isAddPlacePopupOpen} title='Новое место' name='add'
-                    buttonText='Создать' children={
-            <>
+                               buttonText='Создать' children={
+                    <>
 
-                    <label className="form__field">
-                        <input
-                            type="text"
-                            className="form__item form__item_el_place"
-                            id="place"
-                            name="place"
-                            placeholder="Название"
-                            required
-                            minLength="1"
-                            maxLength="30"
-                        />
-                        <div className="form__error-text" id="place-error"></div>
-                    </label>
-                    <label className="form__field">
-                        <input
-                            type="url"
-                            className="form__item form__item_el_img"
-                            id="img"
-                            name="img"
-                            placeholder="Ссылка на картинку"
-                            required
-                        />
-                        <div className="form__error-text" id="img-error"></div>
-                    </label>
+                        <label className="form__field">
+                            <input
+                                type="text"
+                                className="form__item form__item_el_place"
+                                id="place"
+                                name="place"
+                                placeholder="Название"
+                                required
+                                minLength="1"
+                                maxLength="30"
+                            />
+                            <div className="form__error-text" id="place-error"></div>
+                        </label>
+                        <label className="form__field">
+                            <input
+                                type="url"
+                                className="form__item form__item_el_img"
+                                id="img"
+                                name="img"
+                                placeholder="Ссылка на картинку"
+                                required
+                            />
+                            <div className="form__error-text" id="img-error"></div>
+                        </label>
 
-                </>}/>
+                    </>}/>
                 <PopupWithForm onClose={closeAllPopups} isOpen={isEditAvatarPopupOpen} title='Обновить аватар'
                                name='avatar' buttonText='Сохранить' children={<>
                     <label className="form__field">
@@ -117,6 +120,7 @@ function App() {
                     </label>
                 </>}/>
                 <PopupWithForm onClose={closeAllPopups} title='Вы уверены?' name='confirm' buttonText='Да'/>
+                <ImagePopup onClose={closeAllPopups} card={selectedCard}/>
             </div>
 
 
